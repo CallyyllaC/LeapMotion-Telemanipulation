ac2153fba18914e9aebcc4414f6889b21208992b
Callum Dyson-Gainsborough <cgainsborough@hotmail.com>
2020-03-02 14:26:23 +0000

    Added Gripper functionality
    Changed default scale to 2
    added tolerance changing from ui
    Added gripper functionality

--- leap_panda_telemanipulation/scripts/LeapMoveGroup.py
+++ leap_panda_telemanipulation/scripts/LeapMoveGroup.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
+import math
 import sys
-import math
-import rospy
-import moveit_commander
-import geometry_msgs.msg
-from leap_panda_telemanipulation.msg import Modified_leap
-from std_msgs.msg import String
-from moveit_commander.conversions import pose_to_list
 import Tkinter
-from Tkinter import *
-import math
+
+import geometry_msgs.msg
+import moveit_commander
+import numpy as np
+import rospy
+import tf.transformations
+import transforms3d as tr
+from leap_panda_telemanipulation.msg import Modified_leap
+from moveit_commander.conversions import pose_to_list
+from std_msgs.msg import String
+
+#from scipy.spatial.transform import Rotation as R
 
 def all_close(goal, actual, tolerance):
   all_equal = True
@@ -24,22 +28,6 @@ def all_close(goal, actual, tolerance):
   
   return True
 
-#code to convert roll pitch yaw into quaternion
-def Quaternion(pose, roll, pitch, yaw):
-    cy = math.cos(yaw * 0.5)
-    sy = math.sin(yaw * 0.5)
-    cp = math.cos(pitch * 0.5)
-    sp = math.sin(pitch * 0.5)
-    cr = math.cos(roll * 0.5)
-    sr = math.sin(roll * 0.5)
-
-    
-    #pose.orientation.w = cy * cp * cr + sy * sp * sr
-    #pose.orientation.x = cy * cp * sr - sy * sp * cr
-    #pose.orientation.y = sy * cp * sr + cy * sp * cr
-    #pose.orientation.z = sy * cp * cr - cy * sp * sr
-
-    return pose
 
 class LeapMoveGroup(object):
 
@@ -50,22 +38,16 @@ class LeapMoveGroup(object):
     moveit_commander.roscpp_initialize(sys.argv)
     rospy.init_node('LeapMoveGroup', anonymous=True)
     robot = moveit_commander.RobotCommander()
-    print "Groups"
-    print robot.get_group_names()
     print ""
-    print "panda arm group joints"
-    print robot.get_joint_names("panda_arm")
-    print ""
-    print "panda hand group joints"
+    print "panda arm hand group joints"
     print robot.get_joint_names("panda_arm_hand")
-    arm = moveit_commander.MoveGroupCommander("panda_arm")
-    hand = moveit_commander.MoveGroupCommander("panda_arm_hand")
+    arm = moveit_commander.MoveGroupCommander("panda_arm_hand")
 
     # Misc variables
     self.subscriber = None
     self.arm = arm
-    self.hand = hand
-    self.space_modifier = 1
+    self.space_modifier = 2
+    self.tolerance = 0.01
     self.previous_offset = [0,0,0]
     self.offset_x = 0
     self.offset_y = 0
@@ -84,7 +66,7 @@ class LeapMoveGroup(object):
 
     self.default_pose = None
     
-    self.waiting = TRUE
+    self.waiting = True
     print("Staring gui")
     self.leap_window.mainloop()
   
@@ -94,66 +76,74 @@ class LeapMoveGroup(object):
     self.leap_window.title('Tracking Info')
 
     #left hand
-    self.leap_lefthand_label_hand = Label(self.leap_window, text='Left Hand').grid(row=0, column=1)
-    self.gui_left_x = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_x = Label(self.leap_window, text='X:').grid(row=1, column=0) 
-    self.leap_lefthand_label_xv = Label(self.leap_window, textvariable=self.gui_left_x).grid(row=1, column=1)
-    self.gui_left_y = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_y = Label(self.leap_window, text='Y:').grid(row=2, column=0) 
-    self.leap_lefthand_label_yv = Label(self.leap_window, textvariable=self.gui_left_y).grid(row=2, column=1)
-    self.gui_left_z = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_z = Label(self.leap_window, text='Z:').grid(row=3, column=0) 
-    self.leap_lefthand_label_zv = Label(self.leap_window, textvariable=self.gui_left_z).grid(row=3, column=1) 
-    self.gui_left_roll = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_roll = Label(self.leap_window, text='Roll:').grid(row=4, column=0) 
-    self.leap_lefthand_label_rollv = Label(self.leap_window, textvariable=self.gui_left_roll).grid(row=4, column=1)
-    self.gui_left_pitch = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_pitch = Label(self.leap_window, text='Pitch:').grid(row=5, column=0) 
-    self.leap_lefthand_label_pitchv = Label(self.leap_window, textvariable=self.gui_left_pitch).grid(row=5, column=1)
-    self.gui_left_yaw = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_yaw = Label(self.leap_window, text='Yaw:').grid(row=6, column=0) 
-    self.leap_lefthand_label_yawv = Label(self.leap_window, textvariable=self.gui_left_yaw).grid(row=6, column=1) 
-    self.gui_left_grab = StringVar(self.leap_window, "0.00 ")
-    self.leap_lefthand_label_grab = Label(self.leap_window, text='Gesture:').grid(row=7, column=0) 
-    self.leap_lefthand_label_grabv = Label(self.leap_window, textvariable=self.gui_left_grab).grid(row=7, column=1) 
+    self.leap_lefthand_label_hand = Tkinter.Label(self.leap_window, text='Left Hand').grid(row=0, column=1)
+    self.gui_left_x = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_x = Tkinter.Label(self.leap_window, text='X:').grid(row=1, column=0) 
+    self.leap_lefthand_label_xv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_x).grid(row=1, column=1)
+    self.gui_left_y = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_y = Tkinter.Label(self.leap_window, text='Y:').grid(row=2, column=0) 
+    self.leap_lefthand_label_yv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_y).grid(row=2, column=1)
+    self.gui_left_z = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_z = Tkinter.Label(self.leap_window, text='Z:').grid(row=3, column=0) 
+    self.leap_lefthand_label_zv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_z).grid(row=3, column=1) 
+    self.gui_left_roll = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_roll = Tkinter.Label(self.leap_window, text='Roll:').grid(row=4, column=0) 
+    self.leap_lefthand_label_rollv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_roll).grid(row=4, column=1)
+    self.gui_left_pitch = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_pitch = Tkinter.Label(self.leap_window, text='Pitch:').grid(row=5, column=0) 
+    self.leap_lefthand_label_pitchv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_pitch).grid(row=5, column=1)
+    self.gui_left_yaw = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_yaw = Tkinter.Label(self.leap_window, text='Yaw:').grid(row=6, column=0) 
+    self.leap_lefthand_label_yawv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_yaw).grid(row=6, column=1) 
+    self.gui_left_grab = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_lefthand_label_grab = Tkinter.Label(self.leap_window, text='Gesture:').grid(row=7, column=0) 
+    self.leap_lefthand_label_grabv = Tkinter.Label(self.leap_window, textvariable=self.gui_left_grab).grid(row=7, column=1) 
 
     #right hand gui
-    self.leap_righthand_label_hand = Label(self.leap_window, text='Right Hand').grid(row=0, column=2) 
-    self.gui_right_x = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_xv = Label(self.leap_window, textvariable=self.gui_right_x).grid(row=1, column=2)
-    self.gui_right_y  = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_yv = Label(self.leap_window, textvariable=self.gui_right_y).grid(row=2, column=2)
-    self.gui_right_z = StringVar(self.leap_window, "0.00 ") 
-    self.leap_righthand_label_zv = Label(self.leap_window, textvariable=self.gui_right_z).grid(row=3, column=2) 
-    self.gui_right_roll = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_rollv = Label(self.leap_window, textvariable=self.gui_right_roll).grid(row=4, column=2) 
-    self.gui_right_pitch = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_pitchv = Label(self.leap_window, textvariable=self.gui_right_pitch).grid(row=5, column=2) 
-    self.gui_right_yaw = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_yawv = Label(self.leap_window, textvariable=self.gui_right_yaw).grid(row=6, column=2) 
-    self.gui_right_pinch = StringVar(self.leap_window, "0.00 ")
-    self.leap_righthand_label_pinchv = Label(self.leap_window, textvariable=self.gui_right_pinch).grid(row=7, column=2) 
+    self.leap_righthand_label_hand = Tkinter.Label(self.leap_window, text='Right Hand').grid(row=0, column=2) 
+    self.gui_right_x = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_xv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_x).grid(row=1, column=2)
+    self.gui_right_y  = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_yv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_y).grid(row=2, column=2)
+    self.gui_right_z = Tkinter.StringVar(self.leap_window, "0.00 ") 
+    self.leap_righthand_label_zv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_z).grid(row=3, column=2) 
+    self.gui_right_roll = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_rollv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_roll).grid(row=4, column=2) 
+    self.gui_right_pitch = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_pitchv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_pitch).grid(row=5, column=2) 
+    self.gui_right_yaw = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_yawv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_yaw).grid(row=6, column=2) 
+    self.gui_right_pinch = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_righthand_label_pinchv = Tkinter.Label(self.leap_window, textvariable=self.gui_right_pinch).grid(row=7, column=2) 
 
     #offset
-    self.leap_offset_label = Label(self.leap_window, text='Offset').grid(row=8, column=0) 
-    self.leap_offset_x = StringVar(self.leap_window, "0.00 ")
-    self.leap_offset_label_x = Label(self.leap_window, text='X:').grid(row=9, column=0) 
-    self.leap_offset_label_xv = Label(self.leap_window, textvariable=self.leap_offset_x).grid(row=9, column=1) 
-    self.leap_offset_y = StringVar(self.leap_window, "0.00 ")
-    self.leap_offset_label_y = Label(self.leap_window, text='Y:').grid(row=10, column=0) 
-    self.leap_offset_label_yv = Label(self.leap_window, textvariable=self.leap_offset_y).grid(row=10, column=1) 
-    self.leap_offset_z = StringVar(self.leap_window, "0.00 ")
-    self.leap_offset_label_z = Label(self.leap_window, text='Z:').grid(row=11, column=0) 
-    self.leap_offset_label_zv = Label(self.leap_window, textvariable=self.leap_offset_z).grid(row=11, column=1) 
+    self.leap_offset_label = Tkinter.Label(self.leap_window, text='Offset').grid(row=8, column=0) 
+    self.leap_offset_x = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_offset_label_x = Tkinter.Label(self.leap_window, text='X:').grid(row=9, column=0) 
+    self.leap_offset_label_xv = Tkinter.Label(self.leap_window, textvariable=self.leap_offset_x).grid(row=9, column=1) 
+    self.leap_offset_y = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_offset_label_y = Tkinter.Label(self.leap_window, text='Y:').grid(row=10, column=0) 
+    self.leap_offset_label_yv = Tkinter.Label(self.leap_window, textvariable=self.leap_offset_y).grid(row=10, column=1) 
+    self.leap_offset_z = Tkinter.StringVar(self.leap_window, "0.00 ")
+    self.leap_offset_label_z = Tkinter.Label(self.leap_window, text='Z:').grid(row=11, column=0) 
+    self.leap_offset_label_zv = Tkinter.Label(self.leap_window, textvariable=self.leap_offset_z).grid(row=11, column=1) 
     
-    #input scale
-    self.gui_scale = StringVar(self.leap_window, self.space_modifier)
-    self.leap_label_scale = Label(self.leap_window, text='Scale:').grid(row=12, column=0) 
-    self.leap_label_scalev = Entry(self.leap_window, textvariable=self.gui_scale).grid(row=12, column=1) 
+    self.reset_button = Tkinter.Button(self.leap_window, text = "Reset Offset", command = self.reset_off).grid(row=12,column=1)
 
-    self.reset_button = Button(self.leap_window, text = "Reset", command = self.reset_pos).grid(row=13,column=1)
+    #input scale
+    self.gui_scale = Tkinter.StringVar(self.leap_window, self.space_modifier)
+    self.leap_label_scale = Tkinter.Label(self.leap_window, text='Scale:').grid(row=13, column=0) 
+    self.leap_label_scalev = Tkinter.Entry(self.leap_window, textvariable=self.gui_scale).grid(row=13, column=1)
+
+    #movement tolerance
+    self.gui_tolerance = Tkinter.StringVar(self.leap_window, self.space_modifier)
+    self.leap_label_tolerance = Tkinter.Label(self.leap_window, text='Tolerance:').grid(row=14, column=0) 
+    self.leap_label_tolerancev = Tkinter.Entry(self.leap_window, textvariable=self.gui_tolerance).grid(row=14, column=1)
+
+    self.reset_button = Tkinter.Button(self.leap_window, text = "Reset Robot", command = self.reset_pos).grid(row=15,column=1)
 
     self.gui_scale.set(self.space_modifier)
+    self.gui_tolerance.set(self.tolerance)
 
   def updateGui(self):
     self.gui_left_x.set(self.doubleToString(self.leap.left_location[0]))
@@ -176,16 +166,43 @@ class LeapMoveGroup(object):
     self.leap_offset_y.set(self.offset_y) 
     self.leap_offset_z.set(self.offset_z)
     self.space_modifier = float(self.gui_scale.get())
+    self.tolerance = float(self.gui_tolerance.get())
   
   def doubleToString(self, input):
     return round(input, 2)
 
+  #code to convert roll pitch yaw into quaternion
+  def Quaternion(self, pose, roll, pitch, yaw):
+
+    ##tr quarternion accepts pose orientation x,y,z,w format
+    #tmp2 = tr.euler.euler2quat(roll, pitch, yaw)
+    #pose.orientation = geometry_msgs.msg.Quaternion(*tmp2)
+    #flatmat = tr.quaternions.quat2mat([pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w])
+    #print "default pose as quaternion (x,y,z,w): ", pose.orientation.x, " , ", pose.orientation.y, " , ", pose.orientation.z, " , ", pose.orientation.w
+    #print "default pose as eular: ", roll,  " , ",pitch,  " , ",yaw
+    #print "default pose as rotational matrix: ", flatmat
+
+    #rm = np.array([[1, 0, 0],[0, 0, 1],[0, -1, 0]])
+    #tmp = tr.quaternions.mat2quat(rm)
+    #print "tmp: ",tmp
+    #print "tmp2: ",tmp2
+    #out = tr.quaternions.qmult(tmp, tmp2)
+    #print "out: ", out
+    #pose.orientation = geometry_msgs.msg.Quaternion(*out)
+    #print "pose: ", pose.orientation
+
+    
+    #make the orientation the same as the default pose
+    pose.orientation = self.default_pose.pose.orientation
+
+    return pose
+    
   def callback(self, data):
     self.leap = data
     self.updateGui()
 
     if(self.default_pose == None):
-      self.default_pose = self.arm.get_current_pose()
+      self.default_pose = self.arm.get_current_pose("panda_link8")
 
     if self.waiting:
       self.waiting = False
@@ -202,13 +219,22 @@ class LeapMoveGroup(object):
 
   def reset_pos(self):
     self.waiting = False
-    print("Resetting")
-    current_arm_pose = self.arm.get_current_pose()
-    self.arm.set_pose_target(self.default_pose)
+    print("Resetting robot")
+    
+    current_arm_pose = self.arm.get_current_pose("panda_link8")
+    self.arm.set_pose_target(self.default_pose, "panda_link8")
+
     self.arm.go(wait=True)
     self.arm.stop()
     self.waiting = True
-    return all_close(self.default_pose, current_arm_pose, 0.001)
+    return all_close(self.default_pose, current_arm_pose, self.tolerance)
+
+  def reset_off(self):
+    self.waiting = False
+    print("Resetting offset")
+    self.offset_x = 0
+    self.offset_y = 0
+    self.offset_z = 0
 
   def move_workspace(self):
     if(self.leap.grab > 0.9):
@@ -226,7 +252,7 @@ class LeapMoveGroup(object):
 
   def go_to_pose_goal(self):     
     arm_pose_goal = geometry_msgs.msg.Pose()
-    arm_pose_goal = Quaternion(arm_pose_goal,self.leap.right_orientation[0],self.leap.right_orientation[1],self.leap.right_orientation[2])
+    arm_pose_goal = self.Quaternion(arm_pose_goal,self.leap.right_orientation[0],self.leap.right_orientation[1],self.leap.right_orientation[2])
     arm_pose_goal.position.x = ((self.leap.right_location[0])+self.offset_x)*self.space_modifier
     arm_pose_goal.position.z = ((self.leap.right_location[1])+self.offset_y)*self.space_modifier
     arm_pose_goal.position.y = -1*((self.leap.right_location[2])+self.offset_z)*self.space_modifier
@@ -237,16 +263,16 @@ class LeapMoveGroup(object):
     print (arm_pose_goal.orientation.w, arm_pose_goal.orientation.x, arm_pose_goal.orientation.y, arm_pose_goal.orientation.z)
     print ("")
 
-    self.arm.set_pose_target(arm_pose_goal)
+    self.arm.set_pose_target(arm_pose_goal, "panda_link8")
     
     self.arm.go(wait=True)
 
     self.arm.stop()
 
     self.arm.clear_pose_targets()
-    current_arm_pose = self.arm.get_current_pose().pose
+    current_arm_pose = self.arm.get_current_pose("panda_link8").pose
 
-    return all_close(arm_pose_goal, current_arm_pose, 0.001)
+    return all_close(arm_pose_goal, current_arm_pose, self.tolerance)
 
   def go_to_pinch_goal(self):    
     
@@ -254,28 +280,21 @@ class LeapMoveGroup(object):
     print ("Pinching:", self.leap.pinch)
 
 
-    hand_joint_goal = self.hand.get_current_joint_values()
+    hand_joint_goal = self.arm.get_current_joint_values()
     
     print hand_joint_goal
     print ("")
 
-    hand_joint_goal[0] = 0 #self.leap.pinch
-    hand_joint_goal[1] = 0 #self.leap.pinch
-    hand_joint_goal[2] = 0
-    hand_joint_goal[3] = 0
-    hand_joint_goal[4] = 0
-    hand_joint_goal[5] = 0
-    hand_joint_goal[6] = 0
-    hand_joint_goal[7] = 0
-    hand_joint_goal[8] = 0
+    hand_joint_goal[7] = 0.035 - (self.leap.pinch*0.035)
+    hand_joint_goal[8] = 0.035 - (self.leap.pinch*0.035)
 
-    self.hand.go(hand_joint_goal, wait=True)
+    self.arm.go(hand_joint_goal, wait=True)
 
-    self.hand.stop()
+    self.arm.stop()
 
-    current_hand_joints = self.hand.get_current_joint_values()
+    current_hand_joints = self.arm.get_current_joint_values()
 
-    return all_close(hand_joint_goal, current_hand_joints, 0.001)
+    return all_close(hand_joint_goal, current_hand_joints, self.tolerance)
 
 def main():
   try:
@@ -290,13 +309,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
-  #
-  #
-  # ADD PINCHING
-  #
-  # CREATE A NEW LAUNCH FILE
-  #
-  # TEST WITH TWO LEAPS
-  #
-  #
